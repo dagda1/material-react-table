36bc52efc73bf1483f3308b90e67a5fc4afaa560
Paul Cowan <paul.cowan@cutting.scot>
2026-05-22 15:42:46 +0100

    migrate @mui/icons-material imports from individual paths to single barrel import, bump version to 4.0.2

--- packages/material-react-table/src/icons.ts
+++ packages/material-react-table/src/icons.ts
@@ -1,38 +1,4 @@
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import CancelIcon from '@mui/icons-material/Cancel';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ClearAllIcon from '@mui/icons-material/ClearAll';
-import CloseIcon from '@mui/icons-material/Close';
-import ContentCopy from '@mui/icons-material/ContentCopy';
-import DensityLargeIcon from '@mui/icons-material/DensityLarge';
-import DensityMediumIcon from '@mui/icons-material/DensityMedium';
-import DensitySmallIcon from '@mui/icons-material/DensitySmall';
-import DragHandleIcon from '@mui/icons-material/DragHandle';
-import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
-import EditIcon from '@mui/icons-material/Edit';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import FilterListOffIcon from '@mui/icons-material/FilterListOff';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import FullscreenIcon from '@mui/icons-material/Fullscreen';
-import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
-import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
-import LastPageIcon from '@mui/icons-material/LastPage';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import PushPinIcon from '@mui/icons-material/PushPin';
-import RestartAltIcon from '@mui/icons-material/RestartAlt';
-import SaveIcon from '@mui/icons-material/Save';
-import SearchIcon from '@mui/icons-material/Search';
-import SearchOffIcon from '@mui/icons-material/SearchOff';
-import SortIcon from '@mui/icons-material/Sort';
-import SyncAltIcon from '@mui/icons-material/SyncAlt';
-import ViewColumnIcon from '@mui/icons-material/ViewColumn';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-
+import { ArrowDownward as ArrowDownwardIcon, ArrowRight as ArrowRightIcon, Cancel as CancelIcon, ChevronLeft as ChevronLeftIcon, ChevronRight as ChevronRightIcon, ClearAll as ClearAllIcon, Close as CloseIcon, ContentCopy, DensityLarge as DensityLargeIcon, DensityMedium as DensityMediumIcon, DensitySmall as DensitySmallIcon, DragHandle as DragHandleIcon, DynamicFeed as DynamicFeedIcon, Edit as EditIcon, ExpandMore as ExpandMoreIcon, FilterAlt as FilterAltIcon, FilterList as FilterListIcon, FilterListOff as FilterListOffIcon, FirstPage as FirstPageIcon, Fullscreen as FullscreenIcon, FullscreenExit as FullscreenExitIcon, KeyboardDoubleArrowDown as KeyboardDoubleArrowDownIcon, LastPage as LastPageIcon, MoreHoriz as MoreHorizIcon, MoreVert as MoreVertIcon, PushPin as PushPinIcon, RestartAlt as RestartAltIcon, Save as SaveIcon, Search as SearchIcon, SearchOff as SearchOffIcon, Sort as SortIcon, SyncAlt as SyncAltIcon, ViewColumn as ViewColumnIcon, VisibilityOff as VisibilityOffIcon } from '@mui/icons-material';
 export const MRT_Default_Icons = {
   ArrowDownwardIcon,
   ArrowRightIcon,
