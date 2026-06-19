df8f6be2eaa4abbda8b0ce85f07f947a8f128979
Paul Cowan <paul.cowan@cutting.scot>
2026-06-19 18:14:22 +0100

    remove react-transition-group ESM alias from vitest config

--- packages/material-react-table/vitest.config.ts
+++ packages/material-react-table/vitest.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: /^react-transition-group\/(.*)$/,
-        replacement: 'react-transition-group/esm/$1.js',
-      },
-    ],
-  },
   test: {
     environment: 'jsdom',
     globals: false,
